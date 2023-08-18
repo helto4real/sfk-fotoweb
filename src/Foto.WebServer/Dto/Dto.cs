25482c505d5c9636b1710474e60c2645c1447d53
Tomas Hellström <tomas.hellstrom@yahoo.se>
2023-08-18 07:04:33 +0200

    UI refactor complete!

--- src/Foto.WebServer/Dto/Dto.cs
+++ src/Foto.WebServer/Dto/Dto.cs
@@ -8,18 +8,16 @@ public class NewUserInfo
     public string Password { get; set; } = default!;
 
     public string Email { get; set; } = default!;
-    public string FirstName { get; set; } = default!;
-    public string LastName { get; set; } = default!;
 
     public string UrlToken { get; set; } = default!;
 }
+
 public class UserInfo
 {
     public string Username { get; set; } = default!;
     public string Email { get; set; } = default!;
     public bool EmailConfirmed { get; set; }
-    public List<string>? Roles { get; set; } = new();
-    public bool IsInRole(string role) => Roles?.Contains(role) ?? false;
+    public List<string> Roles { get; set; } = new();
 }
 
 public class ExternalUserInfo
